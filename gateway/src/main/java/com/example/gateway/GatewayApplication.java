13b1015874ce2e125d2c572e7677de339e05a642
ghkdwlgns612 <ghkdwlgns612@naver.com>
2022-04-15 16:45:01 +0900

    Docker와 Docker-compose로 이미지생성 후 run

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -12,6 +12,7 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("gateway");
     }
 
 }
